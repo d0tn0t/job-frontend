b21ff60edc69c723f3a3510c1681d635f5116efe
Jefferson de T. Pereira <jefferson.guitar@gmail.com>
2024-01-01 11:54:19 -0300

    feat: Add main dashboard page

---
+++ src/views/dashboard/MainView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ScreenOverlay />
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import ScreenOverlay from "@/components/Overlay.vue";
+
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: { ScreenOverlay },
+  name: "MainView",
+  mounted() {
+    this.$router.push("/dashboard/principal");
+  },
+});
+</script>

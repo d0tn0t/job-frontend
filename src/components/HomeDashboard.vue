e6dba34582ec7a589e40334930ba8702d955c68d
Jefferson de T. Pereira <jefferson.guitar@gmail.com>
2024-01-01 11:51:22 -0300

    feat: Add basic dashboard home panels

---
+++ src/components/HomeDashboard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-row justify="center" class="mt-8">
+      <v-col lg="8" md="8" sm="12">
+        <v-card>
+          <v-card-title> Categorias de produtos </v-card-title>
+          <v-card-text>
+            <v-btn-group>
+              <v-btn to="add-category">Adicionar categoria</v-btn>
+              <v-btn to="list-category">Lista de categorias</v-btn>
+            </v-btn-group>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center" class="mt-8">
+      <v-col lg="8" md="8" sm="12">
+        <v-card>
+          <v-card-title> Produtos </v-card-title>
+          <v-card-text>
+            <v-btn-group>
+              <v-btn to="add-product">Adicionar produto</v-btn>
+              <v-btn to="list-product">Lista de produtos</v-btn>
+            </v-btn-group>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeDashboard",
+});
+</script>

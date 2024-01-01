d369591a27c3c3eafedcb74d92ef0576a3a9a694
Jefferson de T. Pereira <jefferson.guitar@gmail.com>
2024-01-01 11:49:36 -0300

    feat: Add simple overlay notification component

---
+++ src/components/Overlay.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-overlay
+    v-model="is_visible"
+    class="align-center justify-center"
+    persistent
+  >
+    <v-progress-circular
+      indeterminate
+      model-value="20"
+      :size="113"
+      :width="12"
+    ></v-progress-circular>
+  </v-overlay>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ScreenOverlay",
+  data: () => {
+    return {
+      is_visible: false,
+    };
+  },
+  props: {
+    show: Boolean,
+  },
+  watch: {
+    show(newVal) {
+      this.is_visible = newVal;
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
